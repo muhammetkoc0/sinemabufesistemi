487f8f7ac9709db6bcf6408efeca431cd6cf3148
muhammetkoc0 <121392529+muhammetkoc0@users.noreply.github.com>
2023-10-26 02:50:47 +0300

    Add project files.

---
+++ sinemabüfesistemi/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace sinemabüfesistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            groupBox3 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            tutarlab = new Label();
+            kasalab = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            label15 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 224, 192);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(72, 224);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(286, 298);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ürün Miktarı";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Mısır";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Çay";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Su";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 199);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Bilet";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 68);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 36);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(97, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 36);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(97, 150);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 36);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(97, 192);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 36);
+            textBox4.TabIndex = 8;
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = Color.White;
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Controls.Add(kasalab);
+            groupBox3.Controls.Add(tutarlab);
+            groupBox3.Controls.Add(label14);
+            groupBox3.Controls.Add(label13);
+            groupBox3.Location = new Point(854, 224);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(286, 298);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Kasa";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(192, 255, 192);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Location = new Point(461, 224);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(286, 298);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Fiyat Listesi";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(17, 199);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 28);
+            label5.TabIndex = 5;
+            label5.Text = "Bilet=";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(17, 158);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 28);
+            label6.TabIndex = 4;
+            label6.Text = "Su=";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(15, 116);
+            label7.Name = "label7";
+            label7.Size = new Size(60, 28);
+            label7.TabIndex = 3;
+            label7.Text = "Çay=";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(15, 76);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 28);
+            label8.TabIndex = 2;
+            label8.Text = "Mısır=";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(104, 76);
+            label9.Name = "label9";
+            label9.Size = new Size(63, 28);
+            label9.TabIndex = 6;
+            label9.Text = "22 TL";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(104, 116);
+            label10.Name = "label10";
+            label10.Size = new Size(62, 28);
+            label10.TabIndex = 8;
+            label10.Text = "10 TL";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(107, 158);
+            label11.Name = "label11";
+            label11.Size = new Size(51, 28);
+            label11.TabIndex = 10;
+            label11.Text = "5 TL";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(105, 199);
+            label12.Name = "label12";
+            label12.Size = new Size(61, 28);
+            label12.TabIndex = 12;
+            label12.Text = "35 TL";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(56, 136);
+            label13.Name = "label13";
+            label13.Size = new Size(149, 28);
+            label13.TabIndex = 1;
+            label13.Text = "Toplam Tutar=";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(96, 254);
+            label14.Name = "label14";
+            label14.Size = new Size(69, 28);
+            label14.TabIndex = 3;
+            label14.Text = "Kasa=";
+            // 
+            // tutarlab
+            // 
+            tutarlab.AutoSize = true;
+            tutarlab.Location = new Point(211, 136);
+            tutarlab.Name = "tutarlab";
+            tutarlab.Size = new Size(24, 28);
+            tutarlab.TabIndex = 4;
+            tutarlab.Text = "0";
+            tutarlab.Click += label15_Click;
+            // 
+            // kasalab
+            // 
+            kasalab.AutoSize = true;
+            kasalab.Location = new Point(162, 254);
+            kasalab.Name = "kasalab";
+            kasalab.Size = new Size(24, 28);
+            kasalab.TabIndex = 6;
+            kasalab.Text = "0";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Location = new Point(56, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 35);
+            button1.TabIndex = 7;
+            button1.Text = "Hesapla";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(56, 179);
+            button2.Name = "button2";
+            button2.Size = new Size(179, 35);
+            button2.TabIndex = 9;
+            button2.Text = "Temizle";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(796, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(392, 197);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Location = new Point(1, 558);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1203, 60);
+            panel1.TabIndex = 4;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Pristina", 36F, FontStyle.Italic, GraphicsUnit.Point);
+            label15.ForeColor = Color.DodgerBlue;
+            label15.Location = new Point(12, 55);
+            label15.Name = "label15";
+            label15.Size = new Size(759, 79);
+            label15.TabIndex = 5;
+            label15.Text = "KOÇ CİNEMA SATIŞ PANELİ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1200, 630);
+            Controls.Add(label15);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Font = new Font("Corbel", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label kasalab;
+        private Label tutarlab;
+        private Label label14;
+        private Label label13;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label15;
+    }
+}
---
+++ sinemabüfesistemi/Form1.cs
@@ -0,0 +1,25 @@
+namespace sinemabüfesistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
